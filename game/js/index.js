8a1a7d560a37d26212789b4a8ea8cabf87825f2f
Luan Nico <luanpotter27@gmail.com>
2018-12-14 20:52:59 -0200

    rendering full tileset

--- game/js/index.js
+++ game/js/index.js
@@ -17,29 +17,10 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('tileset', 'assets/tileset.png');
 }
 
 function create() {
-    this.add.image(400, 300, 'sky');
-
-    const particles = this.add.particles('red');
-
-    const emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    const logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+    const matrix = [ range(20, 0), range(20, 0), range(20, 1), range(20, 0), range(20, 0) ];
+    this.add.image(400, 300, 'tileset');
 }
